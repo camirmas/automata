6ef2515dc9ef71368739d35fca0c0dc94a4b6a01
Cameron Irmas <cirmas@protonmail.com>
2023-07-16 01:13:33 -0700

    Initial commit: sand!

--- main.lua
+++ main.lua
@@ -30,53 +30,36 @@ palette = {
     peach = {255, 204, 170}
 }
 
-local function get_neighbors(c)
-    local neighbors = {}
-
-    for dx=-1,1 do
-        for dy=-1,1 do
-            if not (dx == 0 and dy == 0) then
-                if c.x > 0 and c.x < width - 1 and c.y > 0 and c.y < height - 1 then
-                    local n = grid[c.x+dx][c.y+dy]
-                    if n.alive then
-                        table.insert(neighbors, n)
-                    end
-                end
-            end
-        end
-    end
-
-    return neighbors
-end
-
 local function create_cell(x, y)
     return {
         x = x,
         y = y,
-        alive = false,
+        type = "none",
         t = 0
     }
 end
 
 local function update_cell(c)
-    local new_c = create_cell(c.x, c.y)
-    new_c.alive = c.alive
-    new_c.t = c.t
-
-    local neighbors = get_neighbors(c)
-    local n_neighbors = #neighbors
-
-    if c.alive and (n_neighbors == 2 or n_neighbors == 3) then
-        new_c.t = c.t + 1
-    elseif not c.alive and (n_neighbors == 3) then
-        new_c.alive = true
-        new_c.t = 0
-    else
-        new_c.alive = false
-        new_c.t = 0
+    -- TODO: improve later
+    if c.y == 0 or c.y == height - 1 or c.x == 0 or c.x == width - 1 then
+        return
     end
 
-    return new_c
+    -- care about neighbors 7, 8, 9
+    local c7 = grid[c.x-1][c.y+1] -- bottom left
+    local c8 = grid[c.x][c.y+1] -- bottom middle
+    local c9 = grid[c.x+1][c.y+1] -- bottom right
+
+    if c8 and c8.type == "none" then
+        grid[c.x][c.y].type = "none"
+        grid[c.x][c.y+1].type = "sand"
+    elseif c7 and c7.type == "none" then
+        grid[c.x][c.y].type = "none"
+        grid[c.x-1][c.y+1].type = "sand"
+    elseif c9 and c9.type == "none" then
+        grid[c.x][c.y].type = "none"
+        grid[c.x+1][c.y+1].type = "sand"
+    end
 end
 
 function love.load()
@@ -87,34 +70,27 @@ function love.load()
         end
     end
 
-    for _=1,starting_num do
-        local rx = math.random(width) - 1
-        local ry = math.random(height) - 1
-        local c = grid[rx][ry]
-        c.alive = true
-    end
-
     love.window.setMode(1920/2, 1920/2, {resizable = true})
 end
 
 function love.update()
-    if t % 10 == 0 and not paused then
-        local updates = {}
+    if not paused then
+        local y = height - 1
 
-        for x=0,width-1 do
-            for y=0,height-1 do
+        while y > 0 do
+            for x=0,width-1 do
                 local c = grid[x][y]
-                updates[{x, y}] = update_cell(c)
-            end
-        end    
 
-        for coord, u in pairs(updates) do
-            grid[coord[1]][coord[2]] = u
+                if c and c.type == "sand" then
+                    update_cell(c)
+                end
+            end
+            y = y - 1
         end
     end
 
     -- Check if the game is paused and the left mouse button is down
-    if paused and love.mouse.isDown(1) then
+    if love.mouse.isDown(1) then
         -- Get the mouse position in window coordinates
         local x, y = love.mouse.getPosition()
         
@@ -136,7 +112,7 @@ function love.update()
                 
                 while true do
                     -- Set the cell at the current position to alive
-                    grid[last_game_x][last_game_y].alive = true
+                    grid[last_game_x][last_game_y].type = "sand"
                     
                     -- Break if the current position is the end position
                     if last_game_x == game_x and last_game_y == game_y then
@@ -186,7 +162,7 @@ function love.draw()
     for x=0,width-1 do
         for y=0,height-1 do
             local c = grid[x][y]
-            if c.alive then
+            if c and c.type ~= "none" then
                 love.graphics.rectangle("fill", c.x, c.y, 1, 1)
                 alive = alive + 1
             end
